38afabe46fda6db414bc5a3f1a6a691089dcc905
unicodeveloper <prosperotemuyiwa@gmail.com>
2016-05-07 13:13:45 +0100

    Initial Commit

---
+++ jusibe.js
@@ -0,0 +1,44 @@
+var request = require('request');
+
+module.exports = function(context, cb) {
+
+  var required_params = ['phoneno', 'message', 'publicKey', 'accessToken'];
+
+  for (var i in required_params) {
+        if (typeof context.data[required_params[i]] === 'undefined') {
+            return cb(null, 'Missing ' + required_params[i] + '. Please supply the value.' );
+        }
+  };
+
+  /**
+   * Data to be sent to the Jusibe SMS API Service
+   * @type Object
+   */
+  var formData = {
+      to: context.data.phoneno,
+      from: 'Nerd Stack',
+      message: context.data.message
+  };
+
+  /**
+   * Make a post request to the Jusibe SMS API Service with the appropriate auth details & formData
+   */
+  request.post({
+    url:'https://jusibe.com/smsapi/send_sms/',
+    formData: formData,
+    auth: {
+    'user' : context.data.publicKey,
+    'password' : context.data.accessToken
+  }},
+  function optionalCallback(err, httpResponse, body) {
+      if (err) {
+          cb('Sending of message failed:', err);
+      }
+
+      cb(null, 'Awesome...Message sent successfully');
+  });
+
+};
+
+
+
